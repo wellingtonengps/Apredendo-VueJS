c11c33023316555141979c2519bc24add209ebf0
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-09-04 22:56:25 -0300

    create project with CLI

---
+++ vuejs-cli/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from "./components/Comments.vue";
+
+export default {
+  name: "App",
+  components: {
+    Comments,
+  },
+};
+</script>
+
+<style></style>
