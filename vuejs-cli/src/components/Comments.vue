7fcb7946851b80694a40d143cc8a6837440c5e2d
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-09-05 22:57:04 -0300

    finished tutorial

--- vuejs-cli/src/components/Comments.vue
+++ vuejs-cli/src/components/Comments.vue
@@ -2,30 +2,14 @@
   <div class="container">
     <h1>Comentários</h1>
     <hr />
-    <div class="form-todo form-group">
-      <p>
-        <input
-          placeholder="nome"
-          type="text"
-          name="author"
-          class="form-control"
-          v-model="name"
-        />
-      </p>
-      <p>
-        <textarea
-          placeholder="Comentário"
-          name="message"
-          class="form-control"
-          v-model="message"
-        ></textarea>
-      </p>
-      <button v-on:click="addComment" type="submit" class="btn btn-primary">
-        Comentar
-      </button>
-    </div>
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
     <div class="list-group" style="margin-top: 15px">
-      <div class="list-group-item" v-for="(comment, index) in allComments">
+      <p v-if="comments.length <= 0">Sem commentários...</p>
+      <div
+        class="list-group-item"
+        v-for="(comment, index) in allComments"
+        :key="comment.id"
+      >
         <span class="comment__author"
           >Autor: <strong>{{ comment.name }}</strong></span
         >
@@ -42,27 +26,25 @@
 </template>
 
 <script>
+import FormTodo from "./FormTodo.vue";
 export default {
+  components: {
+    FormTodo,
+  },
   data() {
     return { comments: [], name: "", message: "" };
   },
   methods: {
-    addComment() {
-      if (this.message.trim() === "") {
-        return;
-      }
-      this.comments.push({ name: this.name, message: this.message });
-      this.name = "";
-      this.message = "";
-    },
     removeComment(index) {
       this.comments.splice(index, 1);
+    },
+    addComment(comment) {
+      this.comments.push(comment);
     },
   },
   computed: {
     allComments() {
-      return;
-      this.comments.map((comment) => ({
+      return this.comments.map((comment) => ({
         ...comment,
         name: comment.name.trim() === "" ? "Anônimo" : comment.name,
       }));
